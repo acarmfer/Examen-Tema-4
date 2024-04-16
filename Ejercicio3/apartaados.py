7c96ad38460e0b37ce6b58c8450d8e4a87651233
acarmfer <acarmfer@myuax.com>
2024-04-16 17:08:54 +0200

    cambios

--- Ejercicio3/apartaados.py
+++ Ejercicio3/apartaados.py
@@ -28,10 +28,10 @@ graph = {
     'Gondor': {'Minas Tirith': 3, 'Moria': 4}
 }
 
-start_city = 'Rivendell'
-end_city = 'Minas Tirith'
+start_city = input("Enter the start city: ")
+end_city = input("Enter the end city: ")
 
 distances = dijkstra(graph, start_city)
 shortest_distance = distances[end_city]
 
-print(f"The shortest distance between {start_city} and {end_city} is {shortest_distance}")z<
+print(f"The shortest distance between {start_city} and {end_city} is {shortest_distance}")
